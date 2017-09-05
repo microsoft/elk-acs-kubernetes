117037b31e3a90907b875b710e657556f100f45f
Yawei Wang <yaweiw@microsoft.com>
2017-09-05 16:46:49 +0800

    add if condition for evt params

--- docker/logstash/run.sh
+++ docker/logstash/run.sh
@@ -15,10 +15,12 @@ log()
 # No EH provided
 echo "input {" > /logstash/config/logstash.conf
 echo "  beats { host => \"0.0.0.0\" port => 5043 tags => ['beats']}" >> /logstash/config/logstash.conf
+echo "if [ ! -z '$EVT_HUB_ACC_KEY' -a '$EVT_HUB_ACC_KEY' != 'undefined' ] && [ ! -z '$EVT_HUB_KEY_NAME' -a '$EVT_HUB_KEY_NAME' != 'undefined' ] && [ ! -z '$EVT_HUB_ENT_PATH' -a '$EVT_HUB_ENT_PATH' == 'undefined' ] && [ ! -z '$EVT_HUB_NS' -a '$EVT_HUB_NS' != 'undefined' ] && [ ! -z '$EVT_HUB_PART' -a '$EVT_HUB_PART' != 'undefined' ]; then" >> /logstash/config/logstash.conf
 for eventHub in $(echo $EVT_HUB_ENT_PATH | sed "s/,/ /g")
 do
     echo "  azurewadeventhub {key => '$EVT_HUB_ACC_KEY' username => '$EVT_HUB_KEY_NAME' eventhub => '$eventHub'  namespace => '$EVT_HUB_NS' partitions => $EVT_HUB_PART tags => ['wad']}" >> /logstash/config/logstash.conf
 done
+echo "fi" >> /logstash/config/logstash.conf
 echo "}" >> /logstash/config/logstash.conf
 echo "output {" >> /logstash/config/logstash.conf
 echo "  if [tags][0] == 'beats' {" >> /logstash/config/logstash.conf
