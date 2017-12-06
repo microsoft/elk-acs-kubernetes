9926ee56125b28970cba63a5a39067a731c9a1d4
Yuwei Zhou <yuwzho@microsoft.com>
2017-12-06 15:58:56 +0800

    try

--- scripts/run.sh
+++ scripts/run.sh
@@ -10,7 +10,7 @@ set -e
 echo $@
 
 log "enter scripts/run.sh"
-while getopts ':d:l:u:p:k:r:a:b:j:q:m:n:o:v:s:c:e:f:g:h:i:t:' arg; do
+while getopts ':d:l:u:p:k:r:a:b:j:q:m:n:o:v:s:c:e:f:g:h:i:t:w:x:y:z:R:' arg; do
      log "arg $arg set with value ${OPTARG}"
      case ${arg} in
         d) masterDns=${OPTARG};;
@@ -35,6 +35,11 @@ while getopts ':d:l:u:p:k:r:a:b:j:q:m:n:o:v:s:c:e:f:g:h:i:t:' arg; do
         h) clientId=${OPTARG};;
         i) clientSecret=${OPTARG};;
         t) tenant=${OPTARG};;
+        w) azureCliendId=${OPTARG};;
+        x) azurePwd=${OPTARG};;
+        y) azureTenant=${OPTARG};;
+        z) subscriptionId=${OPTARG};;
+        R) resourceGroup=${OPTARG};;
      esac
 done
 
@@ -78,6 +83,21 @@ if [ -z ${repositoryUrl} ]; then
     exit 1
 fi
 
+if [ -z ${azureCliendId} ]; then
+    echo 'Azure AD client id is required' >&2
+    exit 1
+fi
+
+if [ -z ${azurePwd} ]; then
+    echo 'Azure AD password is required' >&2
+    exit 1
+fi
+
+if [ -z ${azureTenant} ]; then
+    echo 'Azure AD tenand id is required' >&2
+    exit 1
+fi
+
 if [ -z ${authenticationMode} ]; then
     authenticationMode = 'BasicAuth'
 fi
@@ -106,6 +126,10 @@ export KUBECONFIG=/root/.kube/config
 export HOME=/root
 
 # prerequisites, e.g. docker, openresty
+echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ wheezy main" | sudo tee /etc/apt/sources.list.d/azure-cli.list
+sudo apt-key adv --keyserver packages.microsoft.com --recv-keys 52E16F86FEE04B979B07E28DB02C46DF417A0893
+sudo apt-get -y install apt-transport-https
+
 sudo apt-get -y install software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 wget -qO - https://openresty.org/package/pubkey.gpg | sudo apt-key add -
@@ -113,26 +137,39 @@ sudo add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/u
 sudo add-apt-repository -y "deb http://openresty.org/package/ubuntu $(lsb_release -sc) main"
 sudo apt-get update
 apt-cache policy docker-ce
-sudo apt-get install -y unzip docker-ce openresty apache2-utils
+sudo apt-get install -y unzip docker-ce openresty apache2-utils azure-cli
+
+log "Login azure cli"
+az login --service-principal -u ${azureCliendId} -p ${azurePwd} -t ${azureTenant}
+az account set -s ${subscriptionId}
+
+# Add NSG rule
+# virtualNetwork:22 to Any:*
+nsg=$(az network nsg list -g ${resourceGroup} --output table | grep -o -e 'k8s-master-.*-nsg')
+az network nsg rule create -n ssh --nsg-name ${nsg} --priority 102 -g ${resourceGroup} --protocol TCP --destination-port-range * --source-address-prefix VirtualNetwork
+
+
 
 log "install kubectl"
 # install kubectl
-cd /tmp
-# curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
-# ACS currently support K8s v1.7.7
-curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.7.7/bin/linux/amd64/kubectl
-chmod +x ./kubectl
-sudo mv ./kubectl /usr/local/bin/kubectl
+az acs kubernetes install-cli
+# cd /tmp
+# # curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
+# # ACS currently support K8s v1.7.7
+# curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.7.7/bin/linux/amd64/kubectl
+# chmod +x ./kubectl
+# sudo mv ./kubectl /usr/local/bin/kubectl
+
 
 log "write private key to ${privateKeyFile}"
-export DEBIAN_FRONTEND="noninteractive"
 # write private key
 echo "${privateKey}" | base64 -d | tee ${privateKeyFile}
 chmod 400 ${privateKeyFile}
-
-log "copy ${masterUsername}@${masterUrl}:.kube/config to ${KUBECONFIG}"
-mkdir -p /root/.kube
-scp -o StrictHostKeyChecking=no -i ${privateKeyFile} ${masterUsername}@${masterUrl}:.kube/config ${KUBECONFIG}
+mv ${privateKey} ~/.ssh/id_rsa
+az acs kubernetes get-credentials
+# log "copy ${masterUsername}@${masterUrl}:.kube/config to ${KUBECONFIG}"
+# mkdir -p /root/.kube
+# scp -o StrictHostKeyChecking=no -i ${privateKeyFile} ${masterUsername}@${masterUrl}:.kube/config ${KUBECONFIG}
 kubectl get nodes
 
 log "install helm"
